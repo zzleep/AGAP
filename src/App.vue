fb7d9e766acf41179b5bbde0549d63c2d8055858
zzleep <gamingslifer8@gmail.com>
2026-07-30 20:55:10 +0800

    chore: save local dev edits

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="h-full bg-slate-900 text-slate-100 selection:bg-blue-600 selection:text-white">
+  <div class="h-full w-full bg-slate-900 text-slate-100 selection:bg-blue-600 selection:text-white">
     <UpdateBanner />
     <router-view />
   </div>
